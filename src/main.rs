972ed93fb354b0c218deaae674e8bc7ab39e9701
Arthur Ming <guobin.ming@pwc.com>
2023-06-09 14:35:58 +0800

    feat: read commandline params

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, this is arthur ming's todo cli");
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    println!("{:?}, {:?}", action, item);
 }
