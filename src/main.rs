be76a8a53179ed55ba12c531958f74bcf0416ae3
Arthur Ming <guobin.ming@pwc.com>
2023-06-09 15:07:44 +0800

    feat: add todo work and save to db txt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
+use std::collections::HashMap;
+
+struct Todo {
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+
+    fn insert(&mut self, key: String) {
+        // 在我们的map中新增一个新的元素
+        // 我们默认将其状态值设置为true
+        self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record);
+        }
+        std::fs::write("db.txt", content)
+    }
+}
+
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    println!("{:?}, {:?}", action, item);
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occureed:{}", why),
+        }
+    }
 }
